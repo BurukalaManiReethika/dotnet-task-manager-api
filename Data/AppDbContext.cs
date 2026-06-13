59e0f7ef599c13fb116e5ab4bd228661115b8215
BURUKALA MANI REETHIKA <manireethikab@gmail.com>
2026-06-13 10:58:19 +0530

    Add AppDbContext for Entity Framework Core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using dotnet_task_manager_api.Models;
+
+namespace dotnet_task_manager_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks => Set<TaskItem>();
+    }
+}
